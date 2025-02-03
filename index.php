ef38db9d47ff788577ccb3412c5b81ec50248cdb
Riyanto24434 <aris.riyanto24434@gmail.com>
2025-02-03 15:41:15 +0700

    menambahkan paragraph

---
+++ index.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <h1>Hallo Wordl !!!</h1>
+    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Saepe consequatur, sequi dolorum mollitia eos repellat nam distinctio molestiae unde minima illo tempore placeat! Totam explicabo, facere provident nostrum distinctio eligendi?</p>
+</body>
+</html>
